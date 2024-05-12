20edb3047925ad5993cee76e5d4e2cf2b027b751
Hakam-aldeen-Kh <hakamha8@gmail.com>
2024-05-12 08:29:04 +0300

    update App.js to functional component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
-import './App.css';
-import React, {Component} from 'react';
-import Items from './components/item/items'
-import AddItem from './components/addItem/addItem'
-import Total from './components/total/total'
+import React, { useState } from "react";
+import "./App.css";
+import Items from "./components/item/items";
+import AddItem from "./components/addItem/addItem";
+import Total from "./components/total/total";
 
-class App extends Component {
-  state = {
-    items: [
-      {id:1, product:'Pen', price:2},
-      {id:2, product:'Book', price:10}
-    ]
-  }
+const App = () => {
+  const [items, setItems] = useState([
+    { id: 1, product: "Pen", price: 2, quantity: 1 },
+    { id: 2, product: "Book", price: 10, quantity: 1 },
+  ]);
 
-  deleteItem = (id) => {
-    let items = this.state.items
-    let i = items.findIndex(item => item.id === id)
-    items.splice(i, 1)
-    this.setState({items: items})
-  }
+  const deleteItem = (id) => {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
 
-  addItem = (item) => {
-    this.state.items.length > 0 ? (
-      item.id = this.state.items[this.state.items.length - 1].id + 1 
-    ) : item.id = 1
-    console.log(item.id)
-    let items = this.state.items
-    items.push(item)
-    this.setState({items: items})
-  }
+  const addItem = (item) => {
+    setItems((prevItems) => {
+      const newItem = {
+        ...item,
+        id: prevItems.length > 0 ? prevItems[prevItems.length - 1].id + 1 : 1,
+      };
+      return [...prevItems, newItem];
+    });
+  };
 
-  render() {
-    return (
-      <div className="container">
-        <h1>Product List React App</h1>
-        <div className="table">
-          <Items items={this.state.items} del={this.deleteItem}/>
-          <AddItem add={this.addItem}/>
-          <Total items={this.state.items}/>
-        </div>
+  return (
+    <div className="container">
+      <h1>Product List React App</h1>
+      <div className="table">
+        <Items items={items} del={deleteItem} />
+        <AddItem add={addItem} />
+        <Total items={items} />
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
 export default App;
