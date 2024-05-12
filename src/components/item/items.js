7938bda0ee37011d1ae7c28ecb91e22326d41889
Hakam-aldeen-Kh <hakamha8@gmail.com>
2024-05-12 08:42:57 +0300

    build UI for quantity

--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,32 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
-                <p>Quantity</p>
-            </div>
-            {ListItem}
+  const { items, del, quantity } = props;
+  let length = items.length;
+  const ListItem = length ? (
+    items.map((item) => {
+      return (
+        <div key={item.id} className="item">
+          <p>{item.product}</p>
+          <p>{item.price}</p>
+          <div className="edit">
+            <p>+</p>
+            <p>-</p>
+            <p className="delete" onClick={() => del(item.id)}>
+              &times;
+            </p>
+          </div>
+          <p>{item.quantity}</p>
         </div>
-    )
-}
+      );
+    })
+  ) : (
+    <div className="text">There are no items, Try to add some.</div>
+  );
+  return (
+    <div>
+      <div className="header item">
+        <p>Product</p>
+        <p>Price</p>
+        <p>Edit</p>
+        <p>Quantity</p>
+      </div>
+      {ListItem}
+    </div>
+  );
+};
 
-export default Items
+export default Items;
